7526e36bc77d7bee8fa36a1fee92eec0f538e64f
codericky <thevenom425@gmail.com>
2021-02-02 10:05:24 +0700

    add repo

--- src/pages/submit.js
+++ src/pages/submit.js
@@ -5,34 +5,23 @@ const submit = () => {
 return (
     <>
     <Layout>
-        <center><h1>Booking Hotel Reservation</h1></center>
-        <form action="https://getform.io/f/4ff24713-f674-4ed2-b7f7-09d14682722f" method="POST" className="contact-form">
+    <form action="https://getform.io/f/4ff24713-f674-4ed2-b7f7-09d14682722f" method="POST" className="contact-form">
           <div className="row">
+            <h3>Booking Hotel Reservation</h3>
           </div>
-          <p>Names</p>
           <div className="row">
             <input type="text" placeholder="Name" />
           </div>
-          <p>Name Rooms</p>
-          <div className="row">
-            <input type="text" placeholder="Name Rooms" />
-          </div>
-          <p>How Many Adults</p>
-          <div className="row">
-            <input type="text" placeholder="How many adults" />
-          </div>
-          <p>Email</p>
           <div className="row">
             <input type="email" placeholder="E-mail" />
           </div>
-          <p>Add Message</p>
           <div className="row">
             <textarea
               name="request"
               id="request"
               cols="30"
               rows="10"
-              placeholder="Message add other"
+              placeholder="What can we do for you?"
             ></textarea>
           </div>
           <div className="row">
