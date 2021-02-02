df9c3ca20468b46c7dbc0dee14dcb7c321ba8f46
codericky <thevenom425@gmail.com>
2021-02-02 12:52:47 +0700

    add layout

--- src/pages/submit.js
+++ src/pages/submit.js
@@ -1,10 +1,12 @@
 import React from "react"
-import Navbar from "../components/navbar"
-import Footer from "../components/footer"
+import Layout from "../layouts"
+import FbIcon from "../images/icons/facebook.png"
+import MapIcon from "../images/icons/google-maps.png"
+
 const submit = props => {
 return (
     <>
-    <Navbar />
+         <Layout contactPagePath={props.location.pathname}>
         <form action="https://getform.io/f/4ff24713-f674-4ed2-b7f7-09d14682722f" method="POST" className="contact-form">
           <div className="row">
           <h1>Booking Hotel Reservation</h1>
@@ -52,7 +54,27 @@ return (
             <button type="submit">Send</button>
           </div>
         </form>
-        <Footer />
+        <section className="contact-page-header socials">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.facebook.com/LaVittoriadiMaderno/"
+          >
+            <img src={FbIcon} alt="facebook icon" />
+          </a>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.google.com/maps/place/Hotel+Vittoria/@45.6382209,10.6006442,17z/data=!3m1!4b1!4m7!3m6!1s0x0:0xdfe1e097a1faebc6!5m1!1s2018-04-16!8m2!3d45.638064!4d10.602875?hl=it"
+          >
+            <img
+              src={MapIcon}
+              alt="google maps icon"
+              style={{ width: "55px", height: "55px" }}
+            />
+          </a>
+        </section>
+        </Layout>
   </>
   )
 }
