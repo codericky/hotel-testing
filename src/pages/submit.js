3244d71a6ddb7bf6a3e741f4b022330092746b79
codericky <thevenom425@gmail.com>
2021-02-02 12:46:48 +0700

    edit

--- src/pages/submit.js
+++ src/pages/submit.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
-const submit = () => {
+const submit = props => {
 return (
     <>
     <Navbar />
