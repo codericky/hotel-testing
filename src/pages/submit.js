f6473d575d4cc94139c5e55e3104b7563cc8374a
codericky <thevenom425@gmail.com>
2021-02-02 07:35:10 +0700

    edit

--- src/pages/submit.js
+++ src/pages/submit.js
@@ -1,15 +1,15 @@
 import React from "react"
 import Layout from "../layouts"
 
-const submit = props => {
+const submit = () => {
 return (
     <>
-    <Layout contactPagePath={props.location.pathname}>
+    <Layout>
         <center><h1>Booking Hotel Reservation</h1></center>
         <form action="https://getform.io/f/676e5bfd-1154-4ee8-aad5-3568fca6d19e" method="POST" className="contact-form">
           <div className="row">
           </div>
-          <p>Name</p>
+          <p>Names</p>
           <div className="row">
             <input type="text" placeholder="Name" />
           </div>
